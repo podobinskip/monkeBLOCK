3680096411aa6172abb6217d5fd2624efe69d1bb
Patrick Podobinski <pbp5221@psu.edu>
2023-05-20 18:57:58 -0400

    No longer blocks chrome, edge, brave, file prefix

--- background.js
+++ background.js
@@ -5,6 +5,7 @@ var initialTabId;
 var attemptedTabId;
 var currentTabId1;
 var strictMode;
+const prefixes = ["chrome://", "edge://", "brave://", "file:///"]; // Might have to find a different alternative to this.
 
 updateStrictMode(); // KEEP THIS. IT WILL SET STRICTMODE TO FALSE AND NOT UNDEFINED.
 chrome.storage.onChanged.addListener(function (changes, namespace) {
@@ -45,65 +46,67 @@ chrome.runtime.onMessage.addListener(function (message) {
 chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
   if (changeInfo.url) {
     var newDomain = extractDomain(changeInfo.url);
-    if (changeInfo.url.startsWith("chrome://")) {
-      return;
-    } else {
-      const url = new URL(changeInfo.url);
-      if (strictMode === false) {
-        if (url.protocol === "https:" || url.protocol === "http:") {
-          const httpsURL =
-            "https://" + url.hostname + url.pathname + url.search + url.hash;
-          try {
-            const httpsResponse = await fetch(httpsURL);
-            if (httpsResponse.ok) {
-              currentDomain = extractDomain(httpsURL);
-              currentURL = httpsURL;
-              chrome.tabs.update(tabId, { url: currentURL });
-              const sslLabsEndpoint =
-                "https://api.ssllabs.com/api/v3/analyze?host=" + changeInfo.url;
-              try {
-                const response = await fetch(sslLabsEndpoint);
-                const data = await response.json();
-                if (data.endpoints) {
-                  const endpoint = data.endpoints[0];
-                  if (["A+", "A", "A-", "B+", "B"].includes(endpoint.grade)) {
-                    currentDomain = extractDomain(changeInfo.url);
-                    currentURL = changeInfo.url;
-                    chrome.tabs.update(tabId, { url: currentURL });
-                    return;
-                  }
-                }
-              } catch (error) {
-              }
-            }
-          } catch (error) {
-          }
-        }
-      }
-
-      if (newDomain !== currentDomain) {
-        if (tabId !== currentTabId1) {
-          attemptedURL = changeInfo.url;
-          attemptedTabId = tabId;
-          chrome.tabs.remove(tabId);
-        } else {
-          attemptedURL = changeInfo.url;
-          attemptedTabId = tabId;
-          chrome.tabs.update(tabId, { url: currentURL });
-        }
-
-        chrome.notifications.create({
-          title: "monkeBLOCK Alert",
-          message: "A possible unwanted URL was opened: " + changeInfo.url,
-          iconUrl: "monkeBLOCKmain.png",
-          type: "basic",
-          buttons: [{ title: "Stay on this page" }, { title: "Proceed" }],
-        });
-      } else {
-        currentDomain = extractDomain(changeInfo.url);
-        currentURL = changeInfo.url;
+    for (const prefix of prefixes) {
+      if (changeInfo.url.startsWith(prefix)) {
+        return;
       }
     }
+    const url = new URL(changeInfo.url);
+    if (strictMode === false) {
+      if (url.protocol === "https:" || url.protocol === "http:") {
+        const httpsURL =
+          "https://" + url.hostname + url.pathname + url.search + url.hash;
+        try {
+          const httpsResponse = await fetch(httpsURL);
+          if (httpsResponse.ok) {
+            currentDomain = extractDomain(httpsURL);
+            currentURL = httpsURL;
+            chrome.tabs.update(tabId, { url: currentURL });
+            const sslLabsEndpoint =
+              "https://api.ssllabs.com/api/v3/analyze?host=" + changeInfo.url;
+            try {
+              const response = await fetch(sslLabsEndpoint);
+              const data = await response.json();
+              if (data.endpoints) {
+                const endpoint = data.endpoints[0];
+                if (["A+", "A", "A-", "B+", "B"].includes(endpoint.grade)) {
+                  currentDomain = extractDomain(changeInfo.url);
+                  currentURL = changeInfo.url;
+                  chrome.tabs.update(tabId, { url: currentURL });
+                  return;
+                }
+              }
+            } catch (error) {
+            }
+          }
+        } catch (error) {
+        }
+      }
+    }
+
+    if (newDomain !== currentDomain) {
+      if (tabId !== currentTabId1) {
+        attemptedURL = changeInfo.url;
+        attemptedTabId = tabId;
+        chrome.tabs.remove(tabId);
+      } else {
+        attemptedURL = changeInfo.url;
+        attemptedTabId = tabId;
+        chrome.tabs.update(tabId, { url: currentURL });
+      }
+
+      chrome.notifications.create({
+        title: "monkeBLOCK Alert",
+        message: "A possible unwanted URL was opened: " + changeInfo.url,
+        iconUrl: "monkeBLOCKmain.png",
+        type: "basic",
+        buttons: [{ title: "Stay on this page" }, { title: "Proceed" }],
+      });
+    } else {
+      currentDomain = extractDomain(changeInfo.url);
+      currentURL = changeInfo.url;
+    }
+
   }
 });
 
