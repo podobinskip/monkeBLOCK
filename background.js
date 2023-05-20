32f02b7c052a77bbb4086621d7d590b9d896e8c4
Patrick Podobinski <pbp5221@psu.edu>
2023-05-19 21:03:08 -0400

    Protocol+SSL Check, improved re-directer

--- background.js
+++ background.js
@@ -5,6 +5,7 @@ var initialTabId;
 var attemptedTabId;
 var currentTabId1;
 
+
 chrome.tabs.onActivated.addListener(function(activeInfo) {
   var currentTabId = activeInfo.tabId;
   chrome.windows.getCurrent(function(currentWindow) {
@@ -15,6 +16,7 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
       });
       if (currentTab) {
         var url = currentTab.url;
+        console.log("URL: " + url);
         var domain = extractDomain(url);
         if (url === null || url === undefined || url === "") {
           url = currentURL;
@@ -33,48 +35,64 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
 });
 
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(async function(tabId, changeInfo, tab) {
   if (changeInfo.url) {
     console.log("New attempted: " + attemptedURL);
     console.log("current tab: " + currentTabId1);
     console.log("attempted tab: " + tabId);
-    chrome.storage.local.set({ "currentURL": changeInfo.url }, function() {
-      console.log("New URL: " + changeInfo.url);
-    });
     var newDomain = extractDomain(changeInfo.url);
-    if (changeInfo.url.startsWith("chrome://") || currentURL.startsWith("chrome://")) {
+    if (changeInfo.url.startsWith("chrome://") ) {
       return;
-    }
-    if (newDomain !== currentDomain) {
-      if (tabId !== currentTabId1) {
-        console.log("diff tabId");
-        attemptedURL = changeInfo.url;
-        attemptedTabId = tabId;
-        chrome.tabs.remove(tabId);
-      } else {
-        console.log("same tabId");
-        attemptedURL = changeInfo.url;
-        attemptedTabId = tabId;
-        chrome.tabs.update(tabId, { url: currentURL });
-      }
-      console.log("New domain: " + newDomain);
-      chrome.notifications.create({
-        title: "Extension",
-        message: "Hey, this website tried to open this link. Open it?: " + changeInfo.url,
-        iconUrl: "https://cdn.discordapp.com/attachments/1095083022732230786/1108496272727474297/IMG_3011.jpg",
-        type: "basic",
-        buttons: [
-          { title: "No" },
-          { title: "Open" }
-        ]
-      })
     } else {
-      console.log("Same domain: " + newDomain);
-      currentDomain = extractDomain(changeInfo.url);
-      currentURL = changeInfo.url;
+      const url = new URL(changeInfo.url);
+      if (url.protocol === 'https:') {
+        const sslLabsEndpoint = 'https://api.ssllabs.com/api/v3/analyze?host=' + changeInfo.url;
+        const response = await fetch(sslLabsEndpoint);
+        const data = await response.json();
+        if (data.endpoints) {
+          const endpoint = data.endpoints[0];
+          if (['A+', 'A', 'A-', 'B+', 'B'].includes(endpoint.grade)) {
+            console.log("yup!");
+            console.log("Safe domain: " + newDomain);
+          currentDomain = extractDomain(changeInfo.url);
+          currentURL = changeInfo.url;
+            return;
+          }
+        }
+      }
+      console.log("continue!!");
+      if (newDomain !== currentDomain) {
+        if (tabId !== currentTabId1) {
+          console.log("diff tabId");
+          attemptedURL = changeInfo.url;
+          attemptedTabId = tabId;
+          chrome.tabs.remove(tabId);
+        } else {
+          console.log("same tabId");
+          attemptedURL = changeInfo.url;
+          attemptedTabId = tabId;
+          chrome.tabs.update(tabId, { url: currentURL });
+        }
+        console.log("New domain: " + newDomain);
+        chrome.notifications.create({
+          title: "Extension",
+          message: "A possible unwanted URL was opened: " + changeInfo.url,
+          iconUrl: "https://cdn.discordapp.com/attachments/1095083022732230786/1108496272727474297/IMG_3011.jpg",
+          type: "basic",
+          buttons: [
+            { title: "Stay on this page" },
+            { title: "Proceed" }
+          ]
+        });
+      } else {
+        console.log("Same domain: " + newDomain);
+        currentDomain = extractDomain(changeInfo.url);
+        currentURL = changeInfo.url;
+      }
     }
   }
 });
+
 
 
 chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
