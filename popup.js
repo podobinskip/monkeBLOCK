413c6b26b072dee009882fa12fb3e6c25542b4b3
Patrick Podobinski <pbp5221@psu.edu>
2023-05-19 23:06:27 -0400

    Added reload extension button

--- popup.js
+++ popup.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     var strictModeCheckbox = document.getElementById('strict-mode');
     strictModeCheckbox.addEventListener('change', function () {
-      chrome.storage.sync.set({ 'strictModeState': strictModeCheckbox.checked });
+        chrome.storage.sync.set({ 'strictModeState': strictModeCheckbox.checked });
     });
     chrome.storage.sync.get('strictModeState', function (data) {
-      strictModeCheckbox.checked = data.strictModeState;
+        strictModeCheckbox.checked = data.strictModeState;
+    });
+  
+    var reloadButton = document.getElementById('reload-button');
+    reloadButton.addEventListener('click', function () {
+        console.log("RELOADING");
+        chrome.runtime.reload();
     });
   });
   
